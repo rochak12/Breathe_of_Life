778c96c32ea7d856a04c1d1ed374bc8643119e79
Scott_Pierce <scpierce@byui.edu>
2019-06-10 09:27:19 -0600

    created a demo vibration button

--- app/src/main/java/com/cs246/breathoflife/MainActivity.java
+++ app/src/main/java/com/cs246/breathoflife/MainActivity.java
@@ -1,14 +1,30 @@
 package com.cs246.breathoflife;
 
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button vibrate;
+
+    Vibrator v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vibrate = (Button) findViewById(R.id.vibrate);
+        v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        vibrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                v.vibrate(50);
+            }
+        });
 
 
     // scott pierce
