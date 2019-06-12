f55fb8812043ebf8a6b9bb1c8c91597ae14f1839
Grayson <grayclimber@gmail.com>
2019-06-12 12:19:04 -0600

    added a CLICKABLE button

--- app/src/main/java/com/cs246/breathoflife/MainActivity.java
+++ app/src/main/java/com/cs246/breathoflife/MainActivity.java
@@ -1,16 +1,20 @@
 package com.cs246.breathoflife;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button vibrate;
 
     Vibrator v;
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,15 +27,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 v.vibrate(50);
+                Toast.makeText(MainActivity.this, "Vibrate", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Main Button Build
+        Button customButton = findViewById(R.id.exercize_button);
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
             }
         });
 
 
-    // scott pierce
-        // nathan rowley
-    // Grayson Jones
+                // scott pierce
+                // nathan rowley
+                // Grayson Jones
 
 
-        // i am the best
+                // i am the best
     }
 }
