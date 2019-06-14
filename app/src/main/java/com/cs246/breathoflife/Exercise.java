3cea63c0d97339f0deb1f5e7c33e6e5bf5f72608
Rochak Kunwar <rochak.kunwar.rk@gmail.com>
2019-06-14 16:13:15 -0600

    Fixed the issues with timer in Exercise_main class

--- app/src/main/java/com/cs246/breathoflife/Exercise.java
+++ app/src/main/java/com/cs246/breathoflife/Exercise.java
@@ -1,20 +1,25 @@
 package com.cs246.breathoflife;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Exercise extends AppCompatActivity {
 
     private Button HomeBtn;
+    Button startTimerInExercise;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
-
+        startTimerInExercise = findViewById(R.id.button2);
+        textView2 = findViewById(R.id.textView2);
 
         }
 
@@ -22,6 +27,20 @@ public class Exercise extends AppCompatActivity {
 
             finish();
         }
+
+
+        public  void startTimerInExercise(View view){
+            new CountDownTimer(30000, 1000) {
+                public void onTick(long millisUntilFinished) {
+                    textView2.setText(new String("seconds remaining: " + millisUntilFinished / 1000));
+                }
+
+                public void onFinish() {
+                    textView2.setText("done!");
+                }
+            }.start();
+        }
+
 
     };
 
