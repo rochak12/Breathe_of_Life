57ce57e5f6fc58621d11955a0d64de808e972480
Grayson <grayclimber@gmail.com>
2019-06-14 09:27:21 -0600

    buttons working

--- app/src/main/java/com/cs246/breathoflife/Exercise.java
+++ app/src/main/java/com/cs246/breathoflife/Exercise.java
@@ -8,26 +8,21 @@ import android.widget.Button;
 
 public class Exercise extends AppCompatActivity {
 
-    //private Button HomeBtn;
+    private Button HomeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
 
-//        // Create a button to go back to home
-//        HomeBtn = findViewById(R.id.exercize_button);
-//        HomeBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                moveToHome();
-//            }
-//        });
 
-    }
-    // Home Button
-//    private void moveToHome() {
-//        Intent intent = new Intent(Exercise.this, MainActivity.class);
-//        startActivity(intent);
-//    }
-}
+        }
+
+        public void goHome(View view) {
+
+            finish();
+        }
+
+    };
+
+
