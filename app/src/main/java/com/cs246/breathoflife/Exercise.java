29089f2bbe968813a52b955ff11bde9227b77299
Scott_Pierce <scpierce@byui.edu>
2019-06-17 10:00:31 -0600

    working on the vibration, not quite working

--- app/src/main/java/com/cs246/breathoflife/Exercise.java
+++ app/src/main/java/com/cs246/breathoflife/Exercise.java
@@ -2,17 +2,31 @@ package com.cs246.breathoflife;
 
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Exercise extends AppCompatActivity {
 
     private Button HomeBtn;
     Button startTimerInExercise;
     TextView textView2;
+//    private ArrayList<Double> relax = new ArrayList<Double>(
+//            Arrays.<Float>asList(3.0, 3.0)
+//    );
+//    private List<>
+    private List<Integer> relax;
+    Vibrator vi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +34,7 @@ public class Exercise extends AppCompatActivity {
         setContentView(R.layout.activity_exercise);
         startTimerInExercise = findViewById(R.id.button2);
         textView2 = findViewById(R.id.textView2);
-
+        relax = Arrays.asList(3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3);
         }
 
         public void goHome(View view) {
@@ -29,19 +43,33 @@ public class Exercise extends AppCompatActivity {
         }
 
 
-        public  void startTimerInExercise(View view){
-            new CountDownTimer(30000, 1000) {
-                public void onTick(long millisUntilFinished) {
-                    textView2.setText(new String("seconds remaining: " + millisUntilFinished / 1000));
-                }
+        public  void startTimerInExercise(View view) {
+            for (int i = 0; i < relax.size(); i++) {
+                int b = relax.get(i) * 1000;
+//                int c = 1;
+                vi = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 
-                public void onFinish() {
-                    textView2.setText("done!");
-                }
-            }.start();
+                new CountDownTimer(b, 1000) {
+                    int c = 1;
+
+                    public void onTick(long millisUntilFinished) {
+                        if (c == 1)
+                        {
+                            vi.vibrate(50);
+                            c--;
+                        }
+                        textView2.setText(new String("seconds remaining: " + millisUntilFinished / 1000));
+
+                    }
+
+                    public void onFinish() {
+                        textView2.setText("breathe");
+                    }
+                }.start();
+                System.out.println(i);
+            }
         }
 
-
-    };
+    }
 
 
