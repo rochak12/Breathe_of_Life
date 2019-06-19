60a603a1c440d7c348cc7df1e8e9c35b780baa90
Scott_Pierce <scpierce@byui.edu>
2019-06-18 20:35:22 -0600

    got the vibrate working with time!!!

--- app/src/main/java/com/cs246/breathoflife/Exercise.java
+++ app/src/main/java/com/cs246/breathoflife/Exercise.java
@@ -2,6 +2,7 @@ package com.cs246.breathoflife;
 
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -44,28 +46,38 @@ public class Exercise extends AppCompatActivity {
 
 
         public  void startTimerInExercise(View view) {
+
+            vi = (Vibrator) getSystemService(VIBRATOR_SERVICE);
             for (int i = 0; i < relax.size(); i++) {
                 int b = relax.get(i) * 1000;
+                Handler h = new Handler();
+                h.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        vi.vibrate(50);
+                        Toast.makeText(Exercise.this, "Vibrate", Toast.LENGTH_SHORT).show();
+                    }
+                }, b*i);
 //                int c = 1;
-                vi = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-
-                new CountDownTimer(b, 1000) {
-                    int c = 1;
-
-                    public void onTick(long millisUntilFinished) {
-                        if (c == 1)
-                        {
-                            vi.vibrate(50);
-                            c--;
-                        }
-                        textView2.setText(new String("seconds remaining: " + millisUntilFinished / 1000));
-
-                    }
-
-                    public void onFinish() {
-                        textView2.setText("breathe");
-                    }
-                }.start();
+//                vi = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+//
+//                new CountDownTimer(b, 1000) {
+//                    int c = 1;
+//
+//                    public void onTick(long millisUntilFinished) {
+//                        if (c == 1)
+//                        {
+//                            vi.vibrate(50);
+//                            c--;
+//                        }
+//                        textView2.setText(new String("seconds remaining: " + millisUntilFinished / 1000));
+//
+//                    }
+//
+//                    public void onFinish() {
+//                        textView2.setText("breathe");
+//                    }
+//                }.start();
                 System.out.println(i);
             }
         }
