b7ef2d7adc2a8c83aaf6a1e0aafe19dc99c71b61
Grayson <grayclimber@gmail.com>
2019-07-01 10:20:43 -0600

    made swtitches for custom and settings

--- app/src/main/java/com/cs246/breathoflife/MainActivity.java
+++ app/src/main/java/com/cs246/breathoflife/MainActivity.java
@@ -102,11 +102,17 @@ public class MainActivity extends AppCompatActivity {
     //Goes to Custom Activity
     public void launch_Custom(View view){
         System.out.println("Ready to go to Custom Mode");
+        Intent intent = new Intent(MainActivity.this, Custom.class);
+        intent.putExtra(message_Intent, workout_Intent);
+        startActivity(intent);
     }
 
     //Goes to Setting Activity
     public void launch_Setting(View view){
         System.out.println("Ready to go to Setting");
+        Intent intent = new Intent(MainActivity.this, settings.class);
+        intent.putExtra(message_Intent, workout_Intent);
+        startActivity(intent);
     }
 
     //Goes to Info Activity
