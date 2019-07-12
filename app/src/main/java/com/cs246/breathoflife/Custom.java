628aadd705ff1591f1adc2192a6c67384c29f3db
Scott_Pierce <scpierce@byui.edu>
2019-07-12 09:33:14 -0600

    radio buttons assign values

--- app/src/main/java/com/cs246/breathoflife/Custom.java
+++ app/src/main/java/com/cs246/breathoflife/Custom.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,14 +16,14 @@ public class Custom extends AppCompatActivity {
     List<Integer> list;
     List<Integer> mts = Arrays.asList(4000, 4000, 4375, 4375, 4750, 4750, 5125, 5125, 5500, 5500, 5875, 5875);
     List<Integer> fts = Arrays.asList(1000, 1000, 1500, 1500, 2100, 2100, 2700, 2700, 3300, 3300, 3900, 3900, 4500, 4500, 5100, 5100, 5700, 5700);
-    List<Integer> ftm = Arrays.asList(1000, 1000, 1275, 1275, 1550,1550, 1825, 1825, 2100, 2100, 2375, 2375, 2650, 2650, 2925, 2925, 3200, 3200, 3475, 3475, 3750, 3750, 4000, 4000);
+    List<Integer> ftm = Arrays.asList(1000, 1000, 1275, 1275, 1550, 1550, 1825, 1825, 2100, 2100, 2375, 2375, 2650, 2650, 2925, 2925, 3200, 3200, 3475, 3475, 3750, 3750, 4000, 4000);
     List<Integer> stm = Arrays.asList(5875, 5875, 5500, 5500, 5125, 5125, 4750, 4750, 4375, 4375, 4000, 4000);
     List<Integer> stf = Arrays.asList(5700, 5700, 5100, 5100, 4500, 4500, 3900, 3900, 3300, 3300, 2700, 2700, 2100, 2100, 1500, 1500, 1000, 1000);
     List<Integer> mtf = Arrays.asList(4000, 4000, 3750, 3750, 3475, 3475, 3200, 3200, 2925, 2925, 2650, 2650, 2375, 2375, 2100, 2100, 1825, 1825, 1550, 1550, 1275, 1275, 1000, 1000);
     List<Integer> fast1min = Arrays.asList(1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000,
             1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000,
             1000, 1000, 1000, 1000, 1000, 1000, 1000); // enough for 1 minute
-    List<Integer> med1min = Arrays.asList(4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000,4000, 4000, 4000, 4000, 4000, 4000); // 1 minute
+    List<Integer> med1min = Arrays.asList(4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000); // 1 minute
     List<Integer> slow1min = Arrays.asList(6000, 6000, 6000, 6000, 6000, 6000, 6000, 6000, 6000, 6000); // 1 minute
     int fast = 1000;
     int med = 4000;
@@ -38,11 +39,11 @@ public class Custom extends AppCompatActivity {
         setContentView(R.layout.activity_custom);
     }
 
-    protected void defaultAppender(int dur, int type){
+    protected void defaultAppender(int dur, int type) {
         // dur has values 0, 1, or 2.  Depending on the value, it will add
         // that many minutes to the list
-        for (int i = 0; i < dur; i++){
-            switch (type){
+        for (int i = 0; i < dur; i++) {
+            switch (type) {
                 case 1:
                     list.addAll(fast1min);
                     break;
@@ -58,7 +59,7 @@ public class Custom extends AppCompatActivity {
         }
     }
 
-    protected void transitionAppender(int first, int second){
+    protected void transitionAppender(int first, int second) {
         switch (first) {
             case 1:
                 switch (second) {
@@ -93,7 +94,7 @@ public class Custom extends AppCompatActivity {
         }
     }
 
-    protected void createExercise(int duration, int start, int middle, int end){
+    protected void createExercise(int duration, int start, int middle, int end) {
         defaultAppender(duration, start);
         transitionAppender(start, middle);
         defaultAppender(duration, middle);
@@ -106,5 +107,63 @@ public class Custom extends AppCompatActivity {
         Intent intent = new Intent(Custom.this, Exercise.class);
         intent.putExtra(message_Intent, relax_Intent);
         startActivity(intent);
+    }
+
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.three_min:
+                if (checked)
+                    duration = 0;
+                    break;
+            case R.id.six_min:
+                if (checked)
+                    duration = 1;
+                    break;
+            case R.id.nine_min:
+                if (checked)
+                    duration = 2;
+                    break;
+            case R.id.s_slow:
+                if (checked)
+                    start = 1;
+                    break;
+            case R.id.s_med:
+                if (checked)
+                    start = 2;
+                    break;
+            case R.id.s_fast:
+                if (checked)
+                    start = 3;
+                    break;
+            case R.id.m_slow:
+                if (checked)
+                    middle = 1;
+                    break;
+            case R.id.m_med:
+                if (checked)
+                    middle = 2;
+                    break;
+            case R.id.m_fast:
+                if (checked)
+                    middle = 3;
+                    break;
+            case R.id.e_slow:
+                if (checked)
+                    end = 1;
+                    break;
+            case R.id.e_med:
+                if (checked)
+                    end = 2;
+                    break;
+            case R.id.e_fast:
+                if (checked)
+                    end = 3;
+                    break;
+        }
     }
 }
